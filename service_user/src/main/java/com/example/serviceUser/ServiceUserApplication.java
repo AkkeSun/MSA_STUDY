01d21eef517eac0cd83f81f96cfef8a1345a7e17
od <od@sweettracker.co.kr>
2023-11-09 12:02:52 +0900

    perf: service_user feignClient 적용 (dependency 추가)

--- service_user/src/main/java/com/example/serviceUser/ServiceUserApplication.java
+++ service_user/src/main/java/com/example/serviceUser/ServiceUserApplication.java
@@ -3,8 +3,10 @@ package com.example.serviceUser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient // Eureka Client 선언
+@EnableFeignClients // feignClient 사용
 @SpringBootApplication
 public class ServiceUserApplication {
 
