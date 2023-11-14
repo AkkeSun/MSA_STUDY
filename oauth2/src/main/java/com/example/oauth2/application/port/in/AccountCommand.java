7b7d76beab01c10ee5190ec8ee928ceab7219ae1
od <od@sweettracker.co.kr>
2023-11-14 17:22:02 +0900

    feat: oauth2 로그인 성공시 로그인 시간 업데이트 (입력 커멘드 수정)

--- oauth2/src/main/java/com/example/oauth2/application/port/in/AccountCommand.java
+++ oauth2/src/main/java/com/example/oauth2/application/port/in/AccountCommand.java
@@ -3,6 +3,7 @@ package com.example.oauth2.application.port.in;
 import static com.example.oauth2.infrastructure.exception.ApiErrorCode.ADDRESS_IS_NULL;
 import static com.example.oauth2.infrastructure.exception.ApiErrorCode.INVALID_PHONE_NUMBER;
 import static com.example.oauth2.infrastructure.exception.ApiErrorCode.INVALID_ROLE;
+import static com.example.oauth2.infrastructure.exception.ApiErrorCode.LAST_LOGIN_TIME_IS_NULL;
 import static com.example.oauth2.infrastructure.exception.ApiErrorCode.PASSWORD_CHECK_IS_NULL;
 import static com.example.oauth2.infrastructure.exception.ApiErrorCode.PASSWORD_IS_NULL;
 import static com.example.oauth2.infrastructure.exception.ApiErrorCode.PASSWORD_NOT_MATCHING;
@@ -14,6 +15,7 @@ import static com.example.oauth2.infrastructure.exception.ApiErrorCode.USER_ID_I
 
 import com.example.oauth2.adopter.out.Role;
 import com.example.oauth2.infrastructure.exception.ApiException;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.util.StringUtils;
@@ -31,6 +33,7 @@ public class AccountCommand {
     private String address;
     private String snsSync;
     private String snsSecret;
+    private LocalDateTime lastLoginTime;
 
     public void validation() {
         if (!StringUtils.hasText(userId)) {
@@ -104,13 +107,22 @@ public class AccountCommand {
                 throw new ApiException(SNS_SECRET_IS_NULL);
             }
         }
-        if(StringUtils.hasText(password)){
-            if (!password.equals(passwordCheck)){
+        if (StringUtils.hasText(password)) {
+            if (!password.equals(passwordCheck)) {
                 throw new ApiException(PASSWORD_NOT_MATCHING);
             }
         }
         if (Role.getByValue(role) == null) {
             throw new ApiException(INVALID_ROLE);
+        }
+    }
+
+    public void updateLoginTimeValidation() {
+        if (!StringUtils.hasText(userId)) {
+            throw new ApiException(USER_ID_IS_NULL);
+        }
+        if (lastLoginTime == null) {
+            throw new ApiException(LAST_LOGIN_TIME_IS_NULL);
         }
     }
 
