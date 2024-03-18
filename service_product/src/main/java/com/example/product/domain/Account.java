801ee8186fcc3e94cc44ce290f222c596585cdf7
od <od@sweettracker.co.kr>
2024-03-18 15:50:27 +0900

    feat: service_product Account 도메인 수정

--- service_product/src/main/java/com/example/product/domain/Account.java
+++ service_product/src/main/java/com/example/product/domain/Account.java
@@ -1,5 +1,6 @@
 package com.example.product.domain;
 
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,8 +9,24 @@ public class Account {
 
     private String userId;
 
+    private String name;
+
+    private String phoneNumber;
+
+    private String address;
+
+    private String role;
+
+    private LocalDateTime lastLoginTime;
+
     @Builder
-    public Account(String userId) {
+    public Account(String userId, String name, String phoneNumber, String address, String role,
+        LocalDateTime lastLoginTime) {
         this.userId = userId;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.role = role;
+        this.lastLoginTime = lastLoginTime;
     }
 }
